53c84071fda3a97857bae3e7cc0d7788ce002be7
rajul007 <archiegarg100@gmail.com>
2022-09-21 18:33:14 +0530

    Added Noteitem in a separate Note component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -10,7 +10,44 @@ const NoteState = (props)=>{
         "tag": "Anime",
         "date": "2022-09-19T16:48:24.301Z",
         "__v": 0
-      }]
+      },
+      {
+        "_id": "63289d58376592d4e597d2ea",
+        "user": "6326a09894ab8238bc30dff1",
+        "title": "Kimetsu no Yaiba",
+        "description": "Sun Breathing",
+        "tag": "Anime",
+        "date": "2022-09-19T16:48:24.301Z",
+        "__v": 0
+      },
+      {
+        "_id": "63289d58376592d4e597d2ea",
+        "user": "6326a09894ab8238bc30dff1",
+        "title": "Kimetsu no Yaiba",
+        "description": "Sun Breathing",
+        "tag": "Anime",
+        "date": "2022-09-19T16:48:24.301Z",
+        "__v": 0
+      },
+      {
+        "_id": "63289d58376592d4e597d2ea",
+        "user": "6326a09894ab8238bc30dff1",
+        "title": "Kimetsu no Yaiba",
+        "description": "Sun Breathing",
+        "tag": "Anime",
+        "date": "2022-09-19T16:48:24.301Z",
+        "__v": 0
+      },
+      {
+        "_id": "63289d58376592d4e597d2ea",
+        "user": "6326a09894ab8238bc30dff1",
+        "title": "Kimetsu no Yaiba",
+        "description": "Sun Breathing",
+        "tag": "Anime",
+        "date": "2022-09-19T16:48:24.301Z",
+        "__v": 0
+      }
+    ]
 
       const [notes, setNotes] = useState(notesInitial)
     return (
